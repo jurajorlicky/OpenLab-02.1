7b38c9d41d6da760b003aa3b5ef1ddd86b0b3fdf
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:22:51 +0100

    Add project files.

---
+++ OpenLab 02.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OpenLab_02._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] array = { 1.1F, 2.1F, 3.1F, 4.1F, 5.1F };
+            Console.WriteLine(array[1]);
+        }
+    }
+}
